f7bb85372563a11a36da8edc06d5928a110f5119
SergioRC70 <92979321+SergioRC70@users.noreply.github.com>
2021-11-04 23:37:26 +0100

    Cambios nuevas funcionalidades
    Se han añadido las siguientes funcionalidades:
    - Comentarios en el código
    - Opción para descargar información de todos los equipos
    - Comprobación de nombre del equipo y temporada introducida
    - Tratamiento básico de errores

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 22 12:43:06 2021
-
 @author: Alberto Sánchez & Sergio Romero
 """
 
+# Importamos la clase NBAStatsScraper que tiene toda la lógica del scraper
 from scraper import NBAStatsScraper
 
-print("Elija el equipo: ")
-team = input()
-#team = "hawks"
-print("Elija la temporada: ")
-season = input()
-#season = "2021-22"
+import re
+
+# El script pide dos parámetros: el nombre del equipo y la temporada
+
+# Equipo
+team = ""
+teamNames = ['all', 'celtics', 'nets', 'sixers', 'raptors', 'bulls', 'cavaliers', 'pistons', 
+             'pacers', 'bucks', 'hawks', 'hornets', 'heat', 'magic', 'wizards', 'nuggets', 'timberwolves',
+             'thunder', 'blazers', 'jazz', 'warriors', 'clippers', 'suns', 'kings',
+             'rockets', 'grizzlies', 'pelicans', 'spurs']
+while team == "":
+    print("Elija el equipo: ")
+    team = input()
+    # Comprobamos que el nmbre introducido corresponda con el de un equipo válido.
+    if (not team in teamNames):
+        print('Debe introducir un equipo válido:')
+        print(teamNames)
+        team = ""
+
+# Temorada
+pattern = re.compile('[0-9]{4}-[0-9]{2}')
+season = ''
+while season == '':
+    print("Elija la temporada (formato aaaa-aa): ")
+    season = input()
+    # Comprobamos la cadena introducida tiene el formato válido
+    if pattern.match(season) is None:
+        print('Debe introducir una temporda válida')
+        season = ''
+
+# El nombre del fichero será equipo_temporada.csv
 output_file = "%s_%s.csv" % (team, season)
 
+#try:
+# Iniciaizamos la clase NBAStatsScraper
 scraper = NBAStatsScraper(team, season);
+# Cogemos los datos
 scraper.scrape();
+# Exportamos el dataset al fichero csv
 scraper.data2csv(output_file);
+
+print("La información se ha guardado correctamente.")
+#except:
+#    print("Se ha procudido un error durante el proceso.")
--- scraper.py
+++ scraper.py
@@ -10,113 +10,182 @@ from bs4 import BeautifulSoup
 import csv
 
 class NBAStatsScraper():
+    """
+
+    La clase NBAStatsScraper implementa el scraper para la descarga de estadísticas y fotos de 
+    jugadores de la página web www.nba.com. La información se encuentra dentro de la página del
+    equipo de cuyos jugadores nos queremos baja la información.
     
+    La información del equipo y la temporada para la cual vamos a descargar la información se
+    recibirá como parámetro a la hora de inicializar la clase.
+
+    """   
     def __init__(self, team, season):
+        # Inicialización de la clase.
+        self.team = team
+        self.season = season
+        
+        # URL de la página web que contiene la información a descargar. Esta URL se forma con
+        # dos parámetros que se pasan al inicializar la clase: el nombre del equipo y la
+        # temporada
         self.url = "https://www.nba.com/%s/stats?season=%s" % (team, season)
+        
+        # Array donde guardamos la información que nos vamos a descargar de la página.
+        # Cada elemento del array contiene la informción de un jugador.
         self.teamStats = []
 
     def scrape(self):
-        page = requests.get(self.url)
-        soup = BeautifulSoup(page.content, "html.parser")
-        
-        bodyStats = soup.find_all("table", {"class": "season-totals"})[0].find_all("tbody");
+        # El método scrape se encarga de recorrer la página guardando la información en el
+        # atributo teamStats.
 
-        # Para cada jugador, recorremos la las celdas de la fila correspondiente para recoger
-        # todas sus estadísticas
-        for row in bodyStats[0].find_all("tr"):
-            player_name = row.find_all("span", {"class": "playerName"})[0].find("a").find(text=True)
+        def descargarEquipo(soup):
+            # Descarga la información de un equipo específico. La página parseada se pasa en la
+            # variable soup
             
-            tdStats = row.find_all("td", {"class": "gp"})    
-            if tdStats:
-                games_played = tdStats[0].find(text=True)
-            else:
-                games_played = "-"
+            # Las información que necesitamos se encuentra en la talba de clase season-totals
+            bodyStats = soup.find_all("table", {"class": "season-totals"})[0].find_all("tbody");
+    
+            # Para cada jugador, recorremos la las celdas de la fila correspondiente para recoger
+            # todas sus estadísticas.
+            # No todos los jugadores tienen toda la información por lo que debemos controlas posibles
+            # valores vacíos.
+            for row in bodyStats[0].find_all("tr"):
+                # Nombre del jugador
+                player_name = row.find_all("span", {"class": "playerName"})[0].find("a").find(text=True)
                 
-            tdStats = row.find_all("td", {"class": "pts"})
-            if tdStats:
-                points = tdStats[0].find(text=True)
-            else:
-                points = "-"
+                # Partidos jugados
+                tdStats = row.find_all("td", {"class": "gp"})    
+                if tdStats:
+                    games_played = tdStats[0].find(text=True)
+                else:
+                    games_played = "-"
+                    
+                # Puntos
+                tdStats = row.find_all("td", {"class": "pts"})
+                if tdStats:
+                    points = tdStats[0].find(text=True)
+                else:
+                    points = "-"
+                
+                # Porcentaje de tiros de campo
+                tdStats = row.find_all("td", {"class": "fg_pct"})
+                if tdStats:
+                    field_goals_pct = tdStats[0].find(text=True)
+                else:
+                    field_goals_pct = "-"
             
-        
-            tdStats = row.find_all("td", {"class": "fg_pct"})
-            if tdStats:
-                field_goals_pct = tdStats[0].find(text=True)
-            else:
-                field_goals_pct = "-"
-        
-            tdStats = row.find_all("td", {"class": "fg3_pct"})
-            if tdStats:
-                three_point_pct = tdStats[0].find(text=True)
-            else:
-                three_point_pct = "-"
-        
-            tdStats = row.find_all("td", {"class": "ft_pct"})
-            if tdStats:
-                free_throw_pct = tdStats[0].find(text=True)
-            else:
-                free_throw_pct = "-"
-        
-            tdStats = row.find_all("td", {"class": "oreb"})
-            if tdStats:
-                offensive_rebounds = tdStats[0].find(text=True)
-            else:
-                offensive_rebounds = "-"
-        
-            tdStats = row.find_all("td", {"class": "dreb"})
-            if tdStats:
-                deffensive_rebounds = tdStats[0].find(text=True)
-            else:
-                deffensive_rebounds = "-"
-        
-            tdStats = row.find_all("td", {"class": "reb"})
-            if tdStats:
-                rebounds = tdStats[0].find(text=True)
-            else:
-                rebounds = "-"
-        
-            tdStats = row.find_all("td", {"class": "ast"})
-            if tdStats:
-                assists = tdStats[0].find(text=True)
-            else:
-                assists = "-"
-        
-            tdStats = row.find_all("td", {"class": "stl"})
-            if tdStats:
-                steals = tdStats[0].find(text=True)
-            else:
-                steals = "-"
-        
-            tdStats = row.find_all("td", {"class": "tov"})
-            if tdStats:
-                turnovers = tdStats[0].find(text=True)
-            else:
-                turnovers = "-"
-        
-            tdStats = row.find_all("td", {"class": "pf"})
-            if tdStats:
-                fouls = tdStats[0].find(text=True)
-            else:
-                fouls = "-"
-        
-            playerStats = [player_name, games_played, points, field_goals_pct, three_point_pct, free_throw_pct, offensive_rebounds, deffensive_rebounds, rebounds, assists, steals, turnovers, fouls]
-            self.teamStats.append(playerStats)
+                # Porcentaje de tiros de tres
+                tdStats = row.find_all("td", {"class": "fg3_pct"})
+                if tdStats:
+                    three_point_pct = tdStats[0].find(text=True)
+                else:
+                    three_point_pct = "-"
             
-            # Guardamos la foto del jugadores
-            picture_url = "https:" + row.find("img", {"class": "headshot"})["src"]
-            r = requests.get(picture_url, stream = True)
-            if r.status_code == 200:
-                aSplit = picture_url.split('/')
-                ruta = "Pictures/" + aSplit[len(aSplit)-1]
-                print(ruta)
-                with open(ruta, "wb") as picOutput:
-                    for chunk in r:
-                        picOutput.write(chunk)
+                # Porcentaje de tiros libres
+                tdStats = row.find_all("td", {"class": "ft_pct"})
+                if tdStats:
+                    free_throw_pct = tdStats[0].find(text=True)
+                else:
+                    free_throw_pct = "-"
+            
+                # Rebotes ofensivos
+                tdStats = row.find_all("td", {"class": "oreb"})
+                if tdStats:
+                    offensive_rebounds = tdStats[0].find(text=True)
+                else:
+                    offensive_rebounds = "-"
+            
+                # Rebotes defensivos
+                tdStats = row.find_all("td", {"class": "dreb"})
+                if tdStats:
+                    deffensive_rebounds = tdStats[0].find(text=True)
+                else:
+                    deffensive_rebounds = "-"
+            
+                # Rebotes totales
+                tdStats = row.find_all("td", {"class": "reb"})
+                if tdStats:
+                    rebounds = tdStats[0].find(text=True)
+                else:
+                    rebounds = "-"
+            
+                # Asistencias
+                tdStats = row.find_all("td", {"class": "ast"})
+                if tdStats:
+                    assists = tdStats[0].find(text=True)
+                else:
+                    assists = "-"
+            
+                # Recuperaciones
+                tdStats = row.find_all("td", {"class": "stl"})
+                if tdStats:
+                    steals = tdStats[0].find(text=True)
+                else:
+                    steals = "-"
+            
+                # Perdidas
+                tdStats = row.find_all("td", {"class": "tov"})
+                if tdStats:
+                    turnovers = tdStats[0].find(text=True)
+                else:
+                    turnovers = "-"
+            
+                # Faltas personales
+                tdStats = row.find_all("td", {"class": "pf"})
+                if tdStats:
+                    fouls = tdStats[0].find(text=True)
+                else:
+                    fouls = "-"
+            
+                # Guardamos la información recopilada en el atributo teamStats
+                playerStats = [player_name, games_played, points, field_goals_pct, three_point_pct, free_throw_pct, offensive_rebounds, deffensive_rebounds, rebounds, assists, steals, turnovers, fouls]
+                self.teamStats.append(playerStats)
+                
+                # Guardamos la foto del jugadores
+                picture_url = "https:" + row.find("img", {"class": "headshot"})["src"]
+                r = requests.get(picture_url, stream = True)
+                if r.status_code == 200:
+                    # Las fotos se guardan en la carpeta Pictures con el nombre del jugador.
+                    ruta = "Pictures\\" + player_name.replace(" ", "_") + ".png"
+                    with open(ruta, "wb") as picOutput:
+                        for chunk in r:
+                            picOutput.write(chunk)        
+
+        
+        # Si el nombre del equipo no es 'all'
+        if self.team != 'all': 
+            # Cogemos el contenido de la página del equipo
+            url = "https://www.nba.com/%s/stats?season=%s" % (self.team, self.season)
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, "html.parser")
+            descargarEquipo(soup)
+        # Si el nombre es 'all', vamos a descargar la información de todos los equipos
+        else:
+            teamNames = ['celtics', 'nets', 'sixers', 'raptors', 'bulls', 'cavaliers', 'pistons', 
+             'pacers', 'bucks', 'hawks', 'hornets', 'heat', 'magic', 'wizards', 'nuggets', 'timberwolves',
+             'thunder', 'blazers', 'jazz', 'warriors', 'clippers', 'suns', 'kings', 'rockets',
+             'grizzlies', 'pelicans', 'spurs']
+            # Recorremos todos los equipos
+            for x in teamNames:
+                # Cogemos el contenido de la página del equipo
+                print('Guardando información de ' + x)
+                url = "https://www.nba.com/%s/stats?season=%s" % (x, self.season)
+                page = requests.get(url)
+                soup = BeautifulSoup(page.content, "html.parser")
+                descargarEquipo(soup)
+
 
     def data2csv(self, filename):
-        cabecera="Jugador, Partidos, Puntos, FG%, 3PT%, FT%, OffReb, DefReb, Rebotes, Asistencias, Recuperaciones, Perdidas"
+        # El método data2csv guarda toda la información del atributo teamStats en un fichero
+        # CSV
+        
+        # Cabecera del fichero
+        cabecera = ['Jugador', 'Partidos', 'Puntos', 'FG%', '3PT%', 'FT%', 'OffReb', 'DefReb', 'Rebotes', 'Asistencias', 'Recuperaciones', 'Perdidas', 'Faltas']
+        
+        # Creamos el fichero csv.
         with open(filename, 'w', newline='') as csvFile:
             writer = csv.writer(csvFile)
-            writer.writerow([cabecera])
+            writer.writerow(cabecera)
+            # Por cada elemento del array teamStats creamos una línea en el fichero
             for playerStats in self.teamStats:
                 writer.writerow(playerStats)

36acb018af73f0f619838154e766db4c895b1329
AlberSM83 <92687408+AlberSM83@users.noreply.github.com>
2021-11-05 20:36:03 +0100

    Add files via upload

--- practicaSelenium.py
+++ practicaSelenium.py
@@ -12,9 +12,37 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
-
-
 from time import sleep
+import csv
+
+#Función para mostrar todos los jugadores de una posición que paso por parámetro
+def muestraPosicion(combo, posicion):
+    sleep(2)
+    combo.select_by_visible_text(posicion)
+    sleep(5)
+
+    sleep(2)
+    #OJO, FORZAMOS A QUE CAMBIE LA PAGINACIÓN 2 VECES
+    #Si no forzamos el cambio, al cambiar entre posiciones la paginación de 'All' no funciona!!!
+    todaspaginas.select_by_visible_text('1')
+    todaspaginas.select_by_visible_text('All')
+    sleep(5)
+
+#Función para crear un dataset con un nombre concreto y una lista de jugadores    
+def data2csv(filename, rows):
+    with open(filename, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile)
+        for trJugador in rows:
+            # Get the columns (all the column 2)        
+            datosJugador = trJugador.find_elements(By.CLASS_NAME, "text") 
+            nombreJugador=datosJugador[0].text
+            nombreJugador=nombreJugador.replace('\n', ' ').replace('\r', '')
+            equipo=datosJugador[1].text
+            posicion=datosJugador[2].text
+            parametrosJugador=[nombreJugador, equipo, posicion]
+            writer.writerow(parametrosJugador)
+
+
 
 s=Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
@@ -22,43 +50,43 @@ driver = webdriver.Chrome(service=s)
 
 driver.implicitly_wait(5)
 
-"""Abro la página de la NBA"""
+#Abro la página de la NBA
 driver.get("https://www.nba.com/players")
 
-"""Acepto las cookies"""
+#Aceptamos las cookies
 driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]').click()
 
 sleep(2)
 
-
-"""Selecciono el valor que quiero"""
+#Seleccionamos el combo de posiciones para elegir entre bases, aleros y pivots
 comboPosiciones =  Select(driver.find_element(By.NAME, 'POSITION'))
+#Tocamos la paginación para mostrar todos los elementos
 todaspaginas=Select(driver.find_element(By.XPATH, '//*[@title="Page Number Selection Drown Down List"]'))
 
+#Trabajamos con los bases
 
-sleep(2)
-guards=comboPosiciones.select_by_visible_text('Guard')
-sleep(5)
+#Llamamos a la función para mostrar todos los bases sin paginación
+muestraPosicion(comboPosiciones, 'Guard')
+#obtenemos la lista de jugadores
+listaJugadores=driver.find_element(By.CLASS_NAME, 'players-list')
+rows = listaJugadores.find_elements(By.TAG_NAME, "tr") 
+#los exportamos al dataset
+data2csv("Bases.csv", rows)
 
-sleep(2)
-todaspaginas.select_by_visible_text('All')
-sleep(5)
+#Repetimos la operación anterior para aleros
+muestraPosicion(comboPosiciones, 'Forward')
+listaJugadores=driver.find_element(By.CLASS_NAME, 'players-list')
+rows = listaJugadores.find_elements(By.TAG_NAME, "tr")
+data2csv("Aleros.csv", rows)
+
+#Repetimos la operación para pívots
+muestraPosicion(comboPosiciones, 'Center')
+listaJugadores=driver.find_element(By.CLASS_NAME, 'players-list')
+rows = listaJugadores.find_elements(By.TAG_NAME, "tr") 
+data2csv("Pivots.csv", rows)
 
 
-sleep(2)
-forwards=comboPosiciones.select_by_visible_text('Forward')
-sleep(5)
-
-sleep(2)
-todaspaginas.select_by_visible_text('All')
-sleep(5)
 
 
-sleep(2)
-forwards=comboPosiciones.select_by_visible_text('Center')
-sleep(5)
 
-sleep(2)
-todaspaginas.select_by_visible_text('All')
-sleep(5)
 
